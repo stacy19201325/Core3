d35944e2d540faafcff98d422b365e10909b5003
stacy19201325 <stacy_19201325@hotmail.com>
2017-07-28 01:18:57 -0500

    [Added] Signs to Large Windowed Generic House Style 2

--- MMOCoreORB/bin/scripts/object/building/player/gen_large_win_02.lua
+++ MMOCoreORB/bin/scripts/object/building/player/gen_large_win_02.lua
@@ -56,10 +56,17 @@ object_building_player_generic_large_window_s02 = object_building_player_shared_
 		{"private_med_battle_fatigue", 15}
 	},
 	childObjects = {
---			{templateFile = "object/tangible/sign/player/house_address.iff", x = 4.34, z = 3.4, y = 18.40, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
+		{templateFile = "object/tangible/sign/player/house_address.iff", x = 9.73, z = 2.5, y = -11.14, ox = 0, oy = -4.37114, oz = 0, ow = 1, cellid = -1, containmentType = -1},
 			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = 8.16041, z = 0.511061, y = -8.5, ow = 1, ox = 0, oz = -0, oy = -1.35103e-07, cellid = 1, containmentType = -1}
 --			{templateFile = "object/tangible/terminal/terminal_elevator_up.iff", x = -2.66109, z = -0.513074, y = 0.0544102, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1},
 --			{templateFile = "object/tangible/terminal/terminal_elevator_down.iff", x = -2.65908, z = 5.39344, y = -0.0155522, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1}
+	},
+	shopSigns = {
+		{templateFile = "object/tangible/sign/player/house_address.iff", x = 9.73, z = 2.5, y = -11.14, ox = 0, oy = -4.37114, oz = 0, ow = 1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_address"},
+		{templateFile = "object/tangible/sign/player/shop_sign_s01.iff", x = 11.02, z = 0.5, y = -15.47, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign1"},
+		{templateFile = "object/tangible/sign/player/shop_sign_s02.iff", x = 11.02, z = 0.5, y = -15.47, ox = 0, oy = 1, oz = 0, ow =  0, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign2"},
+		{templateFile = "object/tangible/sign/player/shop_sign_s03.iff", x = 11.02, z = 0.5, y = -15.47, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign3"},
+		{templateFile = "object/tangible/sign/player/shop_sign_s04.iff", x = 11.02, z = 0.5, y = -15.47, ox = 0, oy = 0, oz = 0, ow =  1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:shop_sign4"},
 	}
 }
 
