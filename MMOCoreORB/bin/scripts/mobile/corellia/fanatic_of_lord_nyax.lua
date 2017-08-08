def22f226c213b8c334abafdd694c9357d679373
Taylaria <taylaheq@yahoo.com>
2017-08-08 18:15:20 -0500

    [Change] Fanatic of Lord Nyax changes

--- MMOCoreORB/bin/scripts/mobile/corellia/fanatic_of_lord_nyax.lua
+++ MMOCoreORB/bin/scripts/mobile/corellia/fanatic_of_lord_nyax.lua
@@ -4,15 +4,15 @@ fanatic_of_lord_nyax = Creature:new {
 	randomNameTag = true,
 	socialGroup = "followers_of_lord_nyax",
 	faction = "followers_of_lord_nyax",
-	level = 20,
-	chanceHit = 0.33,
+	level = 100,
+	chanceHit = 0.90,
 	damageMin = 180,
-	damageMax = 190,
-	baseXp = 1803,
-	baseHAM = 4500,
-	baseHAMmax = 5500,
-	armor = 0,
-	resists = {0,20,0,25,-1,-1,25,-1,-1},
+	damageMax = 375,
+	baseXp = 9803,
+	baseHAM = 37500,
+	baseHAMmax = 40500,
+	armor = 1,
+	resists = {50,20,5,25,70,-1,25,50,-1},
 	meatType = "",
 	meatAmount = 0,
 	hideType = "",
@@ -31,17 +31,33 @@ fanatic_of_lord_nyax = Creature:new {
 	lootGroups = {
 		{
 			groups = {
-				{group = "junk", chance = 7000000},
-				{group = "pistols", chance = 1000000},
-				{group = "rifles", chance = 1000000},
-				{group = "carbines", chance = 1000000}
-			}
-		}
+				{group = "junk", chance = 5000000},
+				{group = "melee_two_handed", chance = 1500000},
+				{group = "color_crystals", chance = 2000000},
+				{group = "printer_parts", chance = 1500000}
+			},
+			lootChance = 10000000
+		},
+		{
+			groups = {
+				{group = "armor_attachments", chance = 5000000},
+   		            	{group = "clothing_attachments", chance = 5000000}
+			},
+			lootChance = 4000000
+		},
+		{
+			groups = {
+				{group = "crystals_premium", chance = 5500000},
+				{group = "nge_house_bespin", chance = 4500000}
+			},
+			lootChance = 1500000
+		},
 	},
-	weapons = {"pirate_weapons_heavy"},
+	weapons = {"nyax_fanatic_weapons"},
 	conversationTemplate = "",
 	reactionStf = "@npc_reaction/fancy",
-	attacks = merge(brawlermaster,marksmanmaster)
+	attacks = merge(swordsmanmaster,riflemanmaster)
 }
 
 CreatureTemplates:addCreatureTemplate(fanatic_of_lord_nyax, "fanatic_of_lord_nyax")
+
