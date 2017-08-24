2c292d04d5bf570dc1e152cc0da7e2600a9ddefb
stacy19201325 <stacy_19201325@hotmail.com>
2017-08-23 22:36:40 -0500

    [Revert] furniture crate size change and merge Tarkin custom changes back in

--- MMOCoreORB/bin/scripts/object/draft_schematic/furniture/furniture_plants_potted_large.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/furniture/furniture_plants_potted_large.lua
@@ -43,36 +43,32 @@
 
 object_draft_schematic_furniture_furniture_plants_potted_large = object_draft_schematic_furniture_shared_furniture_plants_potted_large:new {
 
-	templateType = DRAFTSCHEMATIC,
+   templateType = DRAFTSCHEMATIC,
 
-	customObjectName = "Large Potted Plant, Style One",
+   customObjectName = "Large Potted Plant, Style One",
 
-	craftingToolTab = 512, -- (See DraftSchematicObjectTemplate.h)
-	complexity = 15,
-	size = 2,
-	factoryCrateSize = 10,
+   craftingToolTab = 512, -- (See DraftSchematicObjectTemplate.h)
+   complexity = 15, 
+   size = 2, 
 
-	xpType = "crafting_structure_general",
-	xp = 160,
+   xpType = "crafting_structure_general", 
+   xp = 160, 
 
-	assemblySkill = "structure_assembly",
-	experimentingSkill = "structure_experimentation",
-	customizationSkill = "structure_customization",
+   assemblySkill = "structure_assembly", 
+   experimentingSkill = "structure_experimentation", 
+   customizationSkill = "structure_customization", 
 
-	customizationOptions = {},
-	customizationStringNames = {},
-	customizationDefaults = {},
+   customizationOptions = {},
+   customizationStringNames = {},
+   customizationDefaults = {},
 
-	ingredientTemplateNames = {"craft_furniture_ingredients_n", "craft_furniture_ingredients_n", "craft_furniture_ingredients_n"},
-	ingredientTitleNames = {"pot", "tree", "greenery"},
-	ingredientSlotType = {0, 0, 0},
-	resourceTypes = {"mineral", "wood", "chemical"},
-	resourceQuantities = {30, 30, 20},
-	contribution = {100, 100, 100},
+   ingredientTemplateNames = {"craft_furniture_ingredients_n", "craft_furniture_ingredients_n", "craft_furniture_ingredients_n"},
+   ingredientTitleNames = {"pot", "tree", "greenery"},
+   ingredientSlotType = {0, 0, 0},
+   resourceTypes = {"mineral", "wood", "chemical"},
+   resourceQuantities = {30, 30, 20},
+   contribution = {100, 100, 100},
 
-	targetTemplate = "object/tangible/furniture/all/frn_all_plant_potted_lg_s1.iff",
-
-<<<<<<< HEAD
    targetTemplate = "object/tangible/furniture/all/frn_all_plant_potted_lg_s1.iff",
 
    additionalTemplates = {
@@ -80,9 +76,5 @@ object_draft_schematic_furniture_furniture_plants_potted_large = object_draft_sc
 		"object/tangible/furniture/all/shared_frn_all_plant_potted_lg_s3.iff",
 		"object/tangible/furniture/all/shared_frn_all_plant_potted_lg_s4.iff",
              }
-
-=======
-	additionalTemplates = {}
->>>>>>> publish9
 }
 ObjectTemplates:addTemplate(object_draft_schematic_furniture_furniture_plants_potted_large, "object/draft_schematic/furniture/furniture_plants_potted_large.iff")
