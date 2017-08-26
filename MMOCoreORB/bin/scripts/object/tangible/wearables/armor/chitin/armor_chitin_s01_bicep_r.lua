35aa36af551ead644e86502d0be68114f57bbb3d
stacy19201325 <stacy_19201325@hotmail.com>
2017-08-26 15:26:56 -0500

    [Fixed] Armor crafting inconsistencies

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/chitin/armor_chitin_s01_bicep_r.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/chitin/armor_chitin_s01_bicep_r.lua
@@ -110,10 +110,10 @@ object_tangible_wearables_armor_chitin_armor_chitin_s01_bicep_r = object_tangibl
 	acid = 15,
 	lightSaber = 0,
 
-	numberExperimentalProperties = {1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1},
+	nnumberExperimentalProperties = {1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1},
 	experimentalProperties = {"XX", "XX", "XX", "OQ", "SR", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "MA", "OQ", "XX", "XX", "OQ", "SR", "XX"},
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "null", "null", "exp_quality", "exp_durability", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
+	experimentalGroupTitles = {"null", "null", "null", "exp_durability", "exp_quality", "exp_durability", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
 	experimentalMin = {0, 0, 0, 1000, 1, 30000, 25, 22, 15, 1, 0, 5, 30000},
 	experimentalMax = {0, 0, 0, 5000, 40, 50000, 15, 13, 15, 1, 0, 55, 50000},
